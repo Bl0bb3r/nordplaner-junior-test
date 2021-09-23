db3716ef9c870fe28a950ca9467161194eeba1fb
Andreas Blaabjerg <Andreas.blaabjerg1@hotmail.com>
2021-09-23 14:19:59 +0200

    front end up and running. Currently working oin scaffolding of site. Added main theme - forgot to install typescript, need reboot.

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
